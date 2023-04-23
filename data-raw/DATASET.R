3ccd9b0be80ebde79eadfec435d74949bb9556a5
robertlennox <robertlennox9@gmail.com>
2023-04-23 09:49:13 +0200

    projection

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,14 +3,17 @@
 setwd("C:/Users/rb501745/OneDrive - Dalhousie University/BTN/BTN/data-raw")
 
 osterfjord<-sf::st_read("fjord.shp")
-usethis::use_data(osterfjord, overwrite = TRUE)
+usethis::use_data(osterfjord, overwrite = TRUE) %>%
+  sf::st_transform(32633)
 
 vosso<-sf::st_read("Boniteringspolygon.shp")
-usethis::use_data(vosso, overwrite = TRUE)
+usethis::use_data(vosso, overwrite = TRUE) %>%
+  sf::st_transform(32633)
 
 suldal<-sf::st_read("suldalmap.kml") %>% dplyr::select(Name)
 usethis::use_data(vosso, overwrite = TRUE)
 
 aurland<-sf::st_read("aurland.shp")
 usethis::use_data(aurland, overwrite = TRUE)
+
 
