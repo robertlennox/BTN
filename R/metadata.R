d6a4eb1f3ef8ef002ba170c41ecf29d345191156
robertlennox <robertlennox9@gmail.com>
2023-05-09 19:48:08 +0200

    fix

--- R/metadata.R
+++ R/metadata.R
@@ -80,7 +80,7 @@ smoltify<-function(meta, receivers, detections) {
     dplyr::filter(!is.na(.data$key) | .data$Vendor=="Vemco") %>%
     dplyr::rename(sensor=.data$key, oid=.data$ID, ID=.data$value) %>%
     mutate(.data$dmy=lubridate::dmy(.data$dmy)) %>%
-    mutate(.data$fatedate=parse_date_time(.data$fatedate, c("dmy", "dmy_HM")))
+    mutate(.data$fatedate=lubridate::parse_date_time(.data$fatedate, c("dmy", "dmy_HM")))
 
   rec<-receivers %>%
     as_tibble %>%
