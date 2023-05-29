626c351981dfb43c47281f8154f0d1a29964590e
robertlennox <robertlennox9@gmail.com>
2023-05-29 07:10:29 -0300

    gantt

--- R/gantt.R
+++ R/gantt.R
@@ -181,7 +181,7 @@ a<-g(project = gantt,
      mark_years=T)
 
 a<-a+
-  ggplot::theme(axis.text.x=element_text(angle=45, hjust=1))+
-  ggplot::theme(text=element_text(size=14))
+  ggplot2::theme(axis.text.x=element_text(angle=45, hjust=1))+
+  ggplot2::theme(text=element_text(size=14))
 
 return(a)}
