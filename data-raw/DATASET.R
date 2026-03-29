7d99f94f2d333b5e7b87057429b549afa5e8c13e
robertlennox <42450990+robertlennox@users.noreply.github.com>
2026-03-29 16:36:46 -0300

    fix again

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,7 +1,5 @@
 ## code to prepare `DATASET` dataset goes here
 
-setwd("C:/Users/rb501745/OneDrive - Dalhousie University/BTN/BTN/data-raw")
-
 require(sf)
 sf::sf_use_s2(FALSE)
 osterfjord<-readRDS("osterfjord.RDS")
