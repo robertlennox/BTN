54e40b25c5a51233ebfad337676dd50e4cc58ece
robertlennox <42450990+robertlennox@users.noreply.github.com>
2026-03-29 16:39:23 -0300

    again

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,10 @@
 ## code to prepare `DATASET` dataset goes here
 
 require(sf)
+
+
+dir.create("inst/extdata", recursive = TRUE, showWarnings = FALSE)
+
 sf::sf_use_s2(FALSE)
 osterfjord<-readRDS("osterfjord.RDS")
 saveRDS(osterfjord, "inst/extdata/osterfjord.rds")
