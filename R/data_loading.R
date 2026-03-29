1acdde87f82ec0db8fe343ca96fc9315f29c5c77
robertlennox <42450990+robertlennox@users.noreply.github.com>
2026-03-29 18:08:16 -0300

    adding data_loading

---
+++ R/data_loading.R
@@ -0,0 +1,61 @@
+
+vosso <- function() {
+  readRDS(
+    system.file("extdata", "vosso.rds", package = "BTN")
+  )
+}
+
+osterfjord <- function() {
+  readRDS(
+    system.file("extdata", "osterfjord.rds", package = "BTN")
+  )
+}
+
+pace <- function() {
+  readRDS(
+    system.file("extdata", "pace.rds", package = "BTN")
+  )
+}
+
+oxytrout <- function() {
+  readRDS(
+    system.file("extdata", "oxytrout.rds", package = "BTN")
+  )
+}
+
+
+otter <- function() {
+  readRDS(
+    system.file("extdata", "otter.rds", package = "BTN")
+  )
+}
+
+radio <- function() {
+  readRDS(
+    system.file("extdata", "radio.rds", package = "BTN")
+  )
+}
+
+aurland <- function() {
+  readRDS(
+    system.file("extdata", "aurland.rds", package = "BTN")
+  )
+}
+
+suldal <- function() {
+  readRDS(
+    system.file("extdata", "suldal.rds", package = "BTN")
+  )
+}
+
+isfjorden2 <- function() {
+  readRDS(
+    system.file("extdata", "isfjorden2.rds", package = "BTN")
+  )
+}
+
+isfjorden <- function() {
+  readRDS(
+    system.file("extdata", "isfjorden.rds", package = "BTN")
+  )
+}
