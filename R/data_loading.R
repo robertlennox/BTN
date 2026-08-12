37306e1d4b8a17657026623d204304f49fa1d00f
robertlennox <42450990+robertlennox@users.noreply.github.com>
2026-08-12 10:49:37 +0200

    fixing vosso

--- R/data_loading.R
+++ R/data_loading.R
@@ -1,71 +1,71 @@
-
+#' @export
 vosso <- function() {
   readRDS(
     system.file("extdata", "vosso.RDS", package = "BTN")
   )
 }
-
+#' @export
 storel <- function() {
   readRDS(
     system.file("extdata", "sl.RDS", package = "BTN")
   )
 }
-
+#' @export
 orkla <- function() {
   readRDS(
     system.file("extdata", "JOdata.RDS", package = "BTN")
   )
 }
-
+#' @export
 osterfjord <- function() {
   readRDS(
     system.file("extdata", "osterfjord.RDS", package = "BTN")
   )
 }
-
+#' @export
 pace <- function() {
   readRDS(
     system.file("extdata", "pace-wp1.RDS", package = "BTN")
   )
 }
-
+#' @export
 oxytrout <- function() {
   readRDS(
     system.file("extdata", "acceleration_MO2hrkg.RDS", package = "BTN")
   )
 }
 
-
+#' @export
 otter <- function() {
   readRDS(
     system.file("extdata", "otter.RDS", package = "BTN")
   )
 }
-
+#' @export
 radio <- function() {
   readRDS(
     system.file("extdata", "salmon-radio.RDS", package = "BTN")
   )
 }
-
+#' @export
 aurland <- function() {
   readRDS(
     system.file("extdata", "aurland.RDS", package = "BTN")
   )
 }
-
+#' @export
 suldal <- function() {
   readRDS(
     system.file("extdata", "suldal.RDS", package = "BTN")
   )
 }
-
+#' @export
 isfjorden2 <- function() {
   readRDS(
     system.file("extdata", "newisfjorden.RDS", package = "BTN")
   )
 }
-
+#' @export
 isfjorden <- function() {
   readRDS(
     system.file("extdata", "isfjorden.RDS", package = "BTN")
